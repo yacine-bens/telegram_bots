353d36010eab32110dee22c80bcd1d5da28eb8ab
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-17 22:25:17 +0100

    Set webhooks endpoint

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const route_forhire = require('./routes/reddit_forhire');
 const route_phones = require('./routes/phone_price');
 const route_words = require('./routes/words_count');
+const { send } = require('process');
 
 const app = express();
 
@@ -14,12 +15,35 @@ app.use('/words', route_words);
 
 const { VERCEL_URL } = process.env;
 
+
+app.get('/setWebhooks', (req, res) => {
+    let response = setWebhooks();
+    return res.send(response.data);
+})
+
 app.listen(process.env.PORT || 5000, async () => {
     console.log('App in running on port:', process.env.PORT || 5000);
-    let response = await axios.get(`https://${VERCEL_URL}/forhire/setWebhook`);
-    console.log(response.data);
-    response = await axios.get(`https://${VERCEL_URL}/phones/setWebhook`);
-    console.log(response.data);
-    response = await axios.get(`https://${VERCEL_URL}/words/setWebhook`);
-    console.log(response.data);
+    setWebhooks();
 });
+
+
+async function setWebhooks() {
+    let response = {};
+
+    let res = await axios.get(`https://${VERCEL_URL}/forhire/setWebhook`);
+    let data = res.data;
+    console.log(data);
+    response = { ...response, data };
+    
+    res = await axios.get(`https://${VERCEL_URL}/phones/setWebhook`);
+    data = res.data;
+    console.log(data);
+    response = { ...response, data };
+    
+    res = await axios.get(`https://${VERCEL_URL}/words/setWebhook`);
+    data = res.data;
+    console.log(data);
+    response = { ...response, data };
+
+    return response;
+}

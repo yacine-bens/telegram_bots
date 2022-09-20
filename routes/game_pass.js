c7c743bf17eba18d60d339889f898309b0801e4c
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-20 13:41:06 +0100

    Added: Coming / Leaving soon games

--- routes/game_pass.js
+++ routes/game_pass.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 // Additional modules
 const jsdom = require('jsdom');
-const { response } = require('express');
 const { JSDOM } = jsdom;
 
 router.use(bodyParser.json());
@@ -39,9 +38,19 @@ router.post(URI, async (req, res) => {
 
     if (isBotCommand(req.body.message)) {
         if (messageText === '/start') response_message = 'Please enter a game name.';
+        else if(messageText === '/coming') {
+            let gamesList = await getGames('coming');
+            gamesList = gamesList['COMING SOON']
+            response_message = formatMessage(gamesList, 'coming', 10);
+        }
+        else if(messageText === '/leaving') {
+            let gamesList = await getGames('leaving');
+            gamesList = gamesList['LEAVING SOON'];
+            response_message = formatMessage(gamesList, 'leaving');
+        }
         else response_message = 'Please enter a valid bot command!';
     }
-    else{
+    else {
         // Send please wait message
         await axios.post(`${TELEGRAM_API}/sendMessage`, {
             chat_id: chatId,
@@ -50,13 +59,13 @@ router.post(URI, async (req, res) => {
 
         let gamesList = await getGames();
         let result = searchGame(messageText, gamesList);
-        if(Object.keys(result).length){
+        if (Object.keys(result).length) {
             response_message = formatMessage(result);
-            if(response_message.length > 8000) response_message = 'Found too many games, please be more specific!';
+            if (response_message.length > 8000) response_message = 'Found too many games, please be more specific!';
         }
         else response_message = `No games found for :\n"${messageText}"`;
     }
-    
+
     //Respond to user
     if (response_message != '') {
         await axios.post(`${TELEGRAM_API}/sendMessage`, {
@@ -80,20 +89,17 @@ function isBotCommand(msg) {
     return false;
 }
 
-async function getGames(){
+async function getGames(category) {
     let res = await axios.get('https://gamepasscounter.com');
     const { document } = (new JSDOM(res.data)).window;
-    
+
     let cards = document.querySelectorAll('.et_pb_row[id] > div');
-    
-    const categories = [
-        "RECENTLY ADDED",
-        "COMING SOON",
-        "LEAVING SOON",
-        "CONSOLE GAMES",
-        "PC GAMES"
-    ]
-    
+
+    let categories = [];
+    if (category === 'coming') categories = ['COMING SOON'];
+    else if (category === 'leaving') categories = ['LEAVING SOON'];
+    else categories = ["RECENTLY ADDED", "COMING SOON", "LEAVING SOON", "CONSOLE GAMES", "PC GAMES"];
+
     let games = [...cards].reduce((acc, cur) => {
         // Category Title
         var categoryTitle = cur.querySelectorAll('.et_pb_text_inner')[1].textContent.toUpperCase();
@@ -101,7 +107,7 @@ async function getGames(){
             var gamesList = [];
             cur.querySelectorAll('.et_pb_toggle_content li a').forEach(link => {
                 let date = link.parentElement.querySelector('b');
-                gamesList.push({ title: link.textContent, url: link.href, date: date ? date.textContent : null })
+                gamesList.push({ title: link.textContent, url: link.href, date: date ? date.textContent.replace(/([^\s\w])/g, '') : null })
             });
             return { ...acc, [categoryTitle]: gamesList };
         }
@@ -120,24 +126,33 @@ function searchGame(gameToBeFound, gamesList) {
                 if (!results[gameTitle]['categories']) results[gameTitle]['categories'] = [];
                 results[gameTitle]['categories'] = [...results[gameTitle]['categories'], category];
                 results[gameTitle]['url'] = game['url'];
-                if(game['date']) results[gameTitle]['date'] = game['date'].replace(/([^\s\w])/g, '');
+                if (game['date']) results[gameTitle]['date'] = game['date'];
             }
         })
     })
     return results;
 }
 
-function formatMessage(games){
+function formatMessage(games, category, count = 100) {
     let result = '';
-    for(let game of Object.keys(games)){
-        result += `<a href="${games[game].url}">${game}</a>\n`;
-        games[game].categories.forEach(cat => {
-            if(cat.includes('SOON')) result += `- ${cat} : ${games[game]['date']}\n`;
-            else result += `- ${cat}\n`;
-        });
-        result += '\n';
+
+    if(category === 'coming' || category === 'leaving'){
+        for(let i = 0; i < count; i++){
+            if(!games[i]) break;
+            result += `<a href="${games[i]['url']}">${games[i]['title']}</a>\n${games[i]['date']}\n\n`;
+        }
     }
-    result += `\nFound ${Object.keys(games).length} games.`
+    else{
+        for (let game of Object.keys(games)) {
+            result += `<a href="${games[game].url}">${game}</a>\n`;
+            games[game].categories.forEach(cat => {
+                if (cat.includes('SOON')) result += `- ${cat} : ${games[game]['date']}\n`;
+                else result += `- ${cat}\n`;
+            });
+            result += '\n';
+        }
+        result += `\nFound ${Object.keys(games).length} games.`
+    }
     return result;
 }
 
